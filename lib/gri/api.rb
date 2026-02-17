2f8f5f886ece0771806cd7db9c8fab8532876eb2
yude <i@yude.jp>
2026-02-17 22:33:37 +0900

    security: validate cast/api input parameters

--- lib/gri/api.rb
+++ lib/gri/api.rb
@@ -3,10 +3,15 @@ require 'gri/updater'
 
 module GRI
   class API
+    NUMBER_RE = /\A-?\d+(?:\.\d+)?\z/
+
     def call env
       if env['PATH_INFO'] =~ %r{^/api/(\w+)/(\w+)/(\w+)\b}
         service_name, section_name, graph_name = $1, $2, $3
         req = Rack::Request.new env
+        num_raw = req['number'].to_s.strip
+        return [400, {}, ["NG\n"]] unless valid_number?(num_raw)
+        num = parse_number num_raw
         root_dir = Config['root-dir'] || Config::ROOT_PATH
         cast_dir = root_dir + '/cast'
         service_dir = "#{cast_dir}/#{service_name}"
@@ -14,7 +19,7 @@ module GRI
         key = "num_#{graph_name}"
         FileUtils.mkdir_p service_dir
 
-        records = [{'_host'=>host, '_key'=>key, 'num'=>req['number']}]
+        records = [{'_host'=>host, '_key'=>key, 'num'=>num}]
         writer = Writer.create 'rrd', :gra_dir=>service_dir, :interval=>60
         writer.write records
         writer.finalize
@@ -23,6 +28,15 @@ module GRI
         res = "NG\n"
       end
       [200, {}, [res]]
+    end
+
+    def valid_number? s
+      return false if s.empty? || s.size > 64
+      s =~ NUMBER_RE
+    end
+
+    def parse_number s
+      s.include?('.') ? s.to_f : s.to_i
     end
   end
 end
--- lib/gri/cast.rb
+++ lib/gri/cast.rb
@@ -9,6 +9,8 @@ require 'gri/clist'
 
 module GRI
   class Cast
+    TOKEN_RE = /\A[-A-Za-z0-9_.]+\z/
+
     def self.parse_path_info env
       if (path_info = env['PATH_INFO']) =~ %r{^/(?:list|graph)\b}
         path_info = Regexp.last_match.post_match
@@ -16,7 +18,14 @@ module GRI
       if path_info
         dummy, service_name, section_name, graph_name = path_info.split /\// #/
       end
+      return nil, nil, nil unless [service_name, section_name, graph_name].compact.all? {|s|
+        valid_token?(s)
+      }
       return service_name, section_name, graph_name
+    end
+
+    def self.valid_token? str
+      str.to_s =~ TOKEN_RE
     end
 
     def initialize
@@ -32,7 +41,8 @@ module GRI
       params = req.params
       cast_dir = Config['cast-dir'] || (Config::ROOT_PATH + '/cast')
       if params['grp']
-        cast_dir = cast_dir + '/' + params['grp']
+        cast_dir = safe_subdir(cast_dir, params['grp'])
+        return [400, {}, ['Bad Request']] unless cast_dir
       end
 
       if (req.query_string =~ /\A(\d+),(\d+)\z/)
@@ -46,6 +56,10 @@ module GRI
       elsif params['search'].to_i == 1
         app = List.new :dirs=>[cast_dir]
       elsif
+        service_name, section_name, graph_name = self.class.parse_path_info(env)
+        if env['PATH_INFO'] =~ %r{^/graph/} and (!service_name or !section_name or !graph_name)
+          return [400, {}, ['Bad Request']]
+        end
         case env['PATH_INFO']
         when %r{^/api/}
           app = API.new
@@ -58,6 +72,14 @@ module GRI
         end
       end
       app.call env
+    end
+
+    def safe_subdir base_dir, name
+      return nil if name.blank?
+      base = File.expand_path(base_dir.to_s)
+      path = File.expand_path(File.join(base, name.to_s))
+      return nil unless path.start_with?(base + '/')
+      path
     end
 
     def public_dir
